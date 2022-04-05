fe461473f4050ee08900de8e8fc1d2e806327124
Claudia Chittim <cchittim10101@gmail.com>
2022-04-04 20:17:30 -0500

    added test files to __test__folder

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -3,3 +3,11 @@ const checkIfEqual = require("../lib/random.js");
 test("checks if 10 is equal to 10", () => {
   expect(checkIfEqual(10, 10)).toBe(true);
 });
+
+module.exports = function (val1, val2) {
+  if (val1 === val2) {
+    return true;
+  } else {
+    return false;
+  }
+};
